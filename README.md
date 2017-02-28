2f091d4e7a40555ed8ee4175e1941c11c4d02e61
ertaquo <ertaquo@nizarium.nizarium.com>
2017-03-01 01:29:04 +0300

    Added --depth and --skip-wildcards arguments, updated README.md

--- README.md
+++ README.md
@@ -1,14 +1,30 @@
 nginx-server-names
 ==================
 
-List server names in nginx.
+Lists server names from nginx config.
 
-Usage: nginx-server-names [nginx-config-file] [--one-line|-1]
+#### Usage ####
 
-Default config file: /etc/nginx.nginx.conf
+`nginx-server-names [nginx-config-file] [--help|-h|-?] [--one-line|-1] [--depth <depth>|-d <depth>] [--skip-wildcard|-w] [--dump]`
 
---one-line | -1   - display all server names in single line
+Default config filename: `/etc/nginx/nginx.conf`
 
+`--help | -h | -?` - display help message
 
+`--one-one | -1` - display all server names in single line
 
-Also see config.h for simple nginx config parser, maybe it will be useful for anyone.
+`--depth <depth> | -d <depth>` - domain name depth (1 for TLDs, 2 for domains, 3 for subdomains etc.)
+
+`--skip-wildcards | -w` - skip server names with wildcards and regular expressions  (RFC 1035 matching only)
+
+`--dump` - dump entire config instead of server names
+
+For example, how to get LetsEncrypt certificates for all domains using certbot in single line:
+
+``certbot certonly --webroot -w /var/www/example/ `nginx-server-names -w -d 2 | sed -e 's/^/-d /' |  paste -sd ' ' -` ``
+
+#### config.h ####
+
+`config.h` file contains simple nginx config parser. See usage example in `main.cpp`.
+
+Maybe it will be useful for somebody else.
--- main.cpp
+++ main.cpp
@@ -12,18 +12,43 @@ using namespace std;
 int main(int argc, char ** argv) {
   const char * config_file = DEFAULT_CONFIG_FILE;
   bool one_line = false;
+  bool use_depth = false;
+  int depth;
+  bool skip_wildcards = false;
   bool dump = false;
 
   for (int i = 1; i < argc; i++) {
-    if (strcmp(argv[i], "--help") == 0) {
-      printf("nginx-server-names [nginx-config-file] [--one-line|-1] [--dump]\n");
-      printf("  Default config file: %s\n", DEFAULT_CONFIG_FILE);
-      printf("  --one-one | -1   - display all server names in single line\n");
-      printf("  --dump           - dump entire config instead of server names\n");
+    if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "-?") == 0) {
+      printf("nginx-server-names [nginx-config-file] [--help|-h|-?] [--one-line|-1] [--depth <depth>|-d <depth>] \n");
+      printf("                   [--skip-wildcard|-w] [--dump]\n\n");
+      printf("  Default config filename: %s\n\n", DEFAULT_CONFIG_FILE);
+      printf("  --help | -h | -?              - display this help\n");
+      printf("  --one-one | -1                - display all server names in single line\n");
+      printf("  --depth <depth> | -d <depth>  - domain name depth (1 for TLDs, 2 for domains, 3 for subdomains etc.)\n");
+      printf("  --skip-wildcards | -w         - skip server names with wildcards and regular expressions \n");
+      printf("                                  (RFC 1035 matching only)\n");
+      printf("  --dump                        - dump entire config instead of server names\n");
       return 0;
     }
     else if (strcmp(argv[i], "--one-line") == 0 || strcmp(argv[i], "-1") == 0) {
       one_line = true;
+    }
+    else if (strcmp(argv[i], "--depth") == 0 || strcmp(argv[i], "-d") == 0) {
+      use_depth = true;
+      if (i + 1 < argc) {
+        i++;
+        depth = atoi(argv[i]);
+      } else {
+        depth = -1;
+      }
+
+      if (depth < 1) {
+        fprintf(stderr, "Invalid depth.\n");
+        return 1;
+      }
+    }
+    else if (strcmp(argv[i], "--skip-wildcards") == 0 || strcmp(argv[i], "-w") == 0) {
+      skip_wildcards = true;
     }
     else if (strcmp(argv[i], "--dump") == 0) {
       dump = true;
@@ -56,6 +81,33 @@ int main(int argc, char ** argv) {
     for (list<string>::iterator ait = (*it)->arguments.begin(); ait != (*it)->arguments.end(); ++ait) {
       string sname = *ait;
       bool exists = false;
+
+      if (skip_wildcards) {
+        bool valid = true;
+        for (string::iterator sit = sname.begin(); sit != sname.end(); ++sit) {
+          char c = *sit;
+          if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || c == '-' || c == '.')
+            continue;
+          valid = false;
+          break;
+        }
+        if (!valid)
+          continue;
+      }
+
+      if (use_depth) {
+        int sname_depth = 0;
+        int i = sname.size();
+        for (string::reverse_iterator rit = sname.rbegin(); rit != sname.rend(); ++rit, i--) {
+          if (*rit == '.') {
+            sname_depth++;
+            if (sname_depth == depth) {
+              sname = sname.substr(i);
+              break;
+            }
+          }
+        }
+      }
 
       if (!server_names.empty())
       for (list<string>::iterator sit = server_names.begin(); sit != server_names.end(); ++sit) {
